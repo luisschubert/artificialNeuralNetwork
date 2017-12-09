457de6a6c3075fdac1d284f26cbe286063587dcb
Luis-Johannes Schubert <luisschubert@users.noreply.github.com>
2017-12-08 21:03:34 -0800

    Update ANN.py
    forward propagation "should" work

--- ANN.py
+++ ANN.py
@@ -13,17 +13,20 @@ class artificialNeuralNetwork:
         self.weightMatrices = []
         for i in range(numberOfLayers-1):
             m = numpy.zeros((numberOfNodesInLayers[i+1], numberOfNodesInLayers[i]))
-            self.
+            m = self.initializeMatrix(m, numberOfNodesInLayers[i])
             self.weightMatrices.append(m)
         pass
 
     def train(self):
-        mnist = file("MNIST/mnist_train.csv")
+        mnist = file("/Users/Student/Desktop/mnist_train.csv")
         for line in iter(mnist):
             pixels = line.split(",")
             digit = pixels.pop(0)
+            inputVector = numpy.array(map(float,pixels))
+            # scale the input vector to be values between (0,1]
+            inputVector = inputVector / 255 * 0.99 +0.01
             # TODO: what format to use here???
-            resultantVector = self.forwardPropagate(pixels)
+            resultantVector = self.forwardPropagate(inputVector)
             self.backwardPropagate(digit, resultantVector)
         mnist.close()
 
@@ -38,14 +41,24 @@ class artificialNeuralNetwork:
     HELPER FUNCTIONS 
     '''
     def forwardPropagate(self, inputVector):
-        pass
+        w1 = self.weightMatrices[0].dot(inputVector)
+        w2 = self.weightMatrices[1].dot(w1)
+        return w2
 
     def backwardPropagate(self, digit, resultantVector):
         pass
 
     def initializeMatrix(self, m, numOfNodes):
+        # w =
+        # i = 0
         for row in range(len(m)):
-            for col in range(len(m[0])):
-                m[row][col] = numpy.random.randn(numOfNodes) * sqrt(2.0/numOfNodes)
+            m[row] = numpy.random.randn(numOfNodes) * sqrt(2.0/numOfNodes)
+            # for col in range(len(m[0])):
+            #     m[row][col] = w[i]
+            #     i = i + 1
+        return m
 
-ann = artificialNeuralNetwork(3,(784,100,10), 100)
+ann = artificialNeuralNetwork(numberOfLayers=3, numberOfNodesInLayers=(784,100,10), numberOfTrainingIterations=100, learningRate=1)
+ann.train()
+
+pass
